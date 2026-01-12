ff77edb32770553e9985f105ca895b80c44ec2c8
shieldst1978 <36296160+shieldst1978@users.noreply.github.com>
2026-01-12 16:41:58 +0000

    updated formatting in scatter_squares.py

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,15 @@
 import matplotlib.pyplot as plt
 
-plt.style.use('seaborn')
+plt.style.use('seaborn-v0_8-whitegrid')
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s=200)
+
+# Set chart title and labels for axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+# Set the size of tick labels.
+ax.tick_params(labelsize=14)
 
 plt.show()
