6179c870a988af6aafeff16284bd37aaa3e50ab2
shieldst1978 <36296160+shieldst1978@users.noreply.github.com>
2026-01-13 17:52:00 +0000

    random walks completed

--- rw_visual.py
+++ rw_visual.py
@@ -1,15 +1,32 @@
+#%%
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as the program is active
 
-# Plot the points in the walk
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
+while True:
+  
+    # Make a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.show()
+    # Plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15,9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds, edgecolors='none', s=1)
+    ax.set_aspect('equal')
+    # Emphasise the first and last points
+    ax.scatter(0,0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolors='none', s=100)
+    
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
+#%%
--- scatter_squares.py
+++ scatter_squares.py
@@ -1,10 +1,16 @@
+#%%
 import matplotlib.pyplot as plt
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8-whitegrid')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and labels for axes.
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style = 'plain')
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of value", fontsize=14)
@@ -12,4 +18,7 @@ ax.set_ylabel("Square of value", fontsize=14)
 # Set the size of tick labels.
 ax.tick_params(labelsize=14)
 
-plt.show()
+#plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
+
+# %%
