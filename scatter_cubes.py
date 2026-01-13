4db02ae6f3330cb581910e2b79941994121fe5fd
shieldst1978 <36296160+shieldst1978@users.noreply.github.com>
2026-01-13 11:14:41 +0000

    playing with cubes and squares

---
+++ scatter_cubes.py
@@ -0,0 +1,24 @@
+#%%
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn-v0_8-whitegrid')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=x_values, cmap=plt.cm.Reds, s=10)
+
+# Set chart title and labels for axes.
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style = 'plain')
+ax.set_title("Cubed Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+# Set the size of tick labels.
+ax.tick_params(labelsize=14)
+
+plt.show()
+#plt.savefig('squares_plot.png', bbox_inches='tight')
+
+# %%
